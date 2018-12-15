4dd0d678394ea394790bfdd1e2bc0fe30f08a379
hansfpc <hansfpc@outlook.com>
2018-12-15 06:33:43 -0300

    Create install-docker.sh

---
+++ scripts/install-docker.sh
@@ -0,0 +1,8 @@
+apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+usermod -a -G docker jenkins
+service jenkins restart
